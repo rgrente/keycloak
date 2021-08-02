3188930ae745a5dcf6fc14bc6f79ec13200e0b84
rgrente <romain.grente@gmail.com>
2021-08-02 14:32:04 +0200

    Update SamlService.java

--- services/src/main/java/org/keycloak/protocol/saml/SamlService.java
+++ services/src/main/java/org/keycloak/protocol/saml/SamlService.java
@@ -460,7 +460,7 @@ public class SamlService extends AuthorizationEndpointBase {
             authSession.setClientNote(SamlProtocol.SAML_BINDING, bindingType);
             authSession.setClientNote(GeneralConstants.RELAY_STATE, relayState);
             authSession.setClientNote(SamlProtocol.SAML_REQUEST_ID, requestAbstractType.getID());
-            authSession.setClientNote(String.join(", ", SamlProtocol.SAML_REQUEST_AUTHNCONTEXT, requestAbstractType.getRequestedAuthnContext().getAuthnContextClassRef().toArray(new String[0])));
+            authSession.setClientNote(SamlProtocol.SAML_REQUEST_AUTHNCONTEXT, requestAbstractType.getRequestedAuthnContext().getAuthnContextClassRef().toArray(new String[0]));
 
             // Handle NameIDPolicy from SP
             NameIDPolicyType nameIdPolicy = requestAbstractType.getNameIDPolicy();
